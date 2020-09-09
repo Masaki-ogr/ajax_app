240de19ce7d93b4bb9c39df09f313a3dd288dcfa
Masaki-ogr <69950491+Masaki-ogr@users.noreply.github.com>
2020-09-09 17:57:25 +0900

    メモ投稿機能のフロントサイドの実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,8 @@
 class PostsController < ApplicationController
+
   def index
     @posts = Post.all.order(id: "DESC")
   end
-
-  # def new
-  # end
 
   def create
     post = Post.create(content: params[:content], checked: false)
@@ -20,6 +18,7 @@ class PostsController < ApplicationController
     end
 
     item = Post.find(params[:id])
-    render json: { post: item }
+    render json:{ post: item }
   end
+
 end
